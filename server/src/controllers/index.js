f83c831fd9d3ddc56de1fcb3fc447ab355a54ca8
Ivaylo Stoyanov <ivailo.stoyanov@dreamix.eu>
2023-02-13 00:51:34 +0200

    feat: access to spotify api and endpoints to fetch song genres, and songs from playlist

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -1,9 +1,69 @@
+const axios = require('axios');
+const queryString = require('node:querystring');
+
+const BASE64_AUTH = Buffer.from(
+  process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_SECRET
+).toString('base64');
+
 const home = async (req, res) => {
   res.send(
-    'Welcome to the Mocked Gmail API!\nCreated by Atanas Ivanov & Ivaylo Stoyanov\n@FMI'
+    'Welcome to the Song guesser API!\nCreated by Atanas Ivanov & Ivaylo Stoyanov\n@FMI'
   );
 };
 
+const getToken = async () => {
+  return await axios.post(
+    'https://accounts.spotify.com/api/token',
+    queryString.stringify({
+      grant_type: 'client_credentials',
+    }),
+    {
+      headers: {
+        Authorization: `Basic ${BASE64_AUTH}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    }
+  );
+};
+
+const genres = async (req, res) => {
+  try {
+    const token = (await getToken()).data.access_token;
+    const genresResponse = await axios.get(
+      'https://api.spotify.com/v1/recommendations/available-genre-seeds',
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    res.send(genresResponse.data.genres);
+  } catch (error) {
+    res.send(error.message);
+  }
+};
+
+const tracks = async (req, res) => {
+  try {
+    const token = (await getToken()).data.access_token;
+    const tracksResponse = await axios.get(
+      'https://api.spotify.com/v1/playlists/4jhLkh8niYrv02FYCxQv7n?si/tracks',
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const tracks = tracksResponse.data.tracks.items;
+    const tracksIds = tracks.map((element) => element.track.id);
+    res.send(tracksIds);
+  } catch (error) {
+    res.send(error);
+  }
+};
+
 module.exports = {
-  home
+  home,
+  genres,
+  tracks,
 };
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,5 +4,7 @@ const controllers = require('../controllers');
 const router = express.Router();
 
 router.get('/', controllers.home);
+router.get('/song-genres', controllers.genres);
+router.get('/songs', controllers.tracks);
 
 module.exports = router;
