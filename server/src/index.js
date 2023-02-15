eb31fb19b0a3d32f7940689cd10a26904515543d
Ivaylo Stoyanov <ivailo.stoyanov@dreamix.eu>
2023-02-15 18:23:39 +0200

    Fixed CORS issue, backend.

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,10 @@
 const app = require('./app');
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  },
+});
 const setupSocketConnections = require('./socket');
 const port = process.env.PORT || 3000;
 
