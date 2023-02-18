62912b567a82cd3e9753f3843a12bfad96f0f53a
Ivaylo Stoyanov <ivailo.stoyanov@dreamix.eu>
2023-02-18 02:31:58 +0200

    finished game flow skeleton

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,11 +1,33 @@
 const mainContent = document.getElementById('mainContent');
 const gameContent = document.getElementById('gameContent');
 const songs = [];
+let socket = null;
 
 const prepareSongs = () => {
 	fetch('http://localhost:3000/songs')
 		.then(response => response.json())
-		.then(data => songs.push(...data));
+		.then(data => songs.push(...data))
+	.catch(error => console.error(error));;
+}
+
+const handlePlayNextSong = () => {
+	if (!songs?.length) {
+		return;
+	}
+	const song = songs[songs.length - 1];
+	var queryString = Object.keys(song).map(key => key + '=' + song[key]).join('&');
+	
+	fetch(`http://localhost:3000/song?${queryString}`)
+		.then(response => response.blob())
+		.then(blob => {
+			const url = URL.createObjectURL(blob);
+			const audio = document.getElementById('audioPlayer');
+			audio.src = url;
+			audio.play();
+		})
+	.catch(error => console.error(error));
+	
+	songs.pop();
 }
 
 const clearMainContent = () => {
@@ -24,13 +46,17 @@ const handleGameStarting = (data) => {
 
 	setTimeout(() => {
 		gameStarting.style.display = 'none';
-
 		handleShowSong(data);
-	}, 3000);
+	}, 1000);
 }
 
 const handleShowSong = ({clientId}) => {
-	const playingAgainst = document.createElement('h2')
+	const playingAgainst = document.createElement('h2');
+	const guessRightBtn = document.createElement('button');
+	guessRightBtn.innerText = 'GUESS';
+	guessRightBtn.addEventListener('click', () => {
+		socket.emit('guess');
+	})
 	playingAgainst.innerText = `You are playing against client: ${clientId}`;
 	
 	const songPlayingHeader = document.createElement('h1');
@@ -38,15 +64,17 @@ const handleShowSong = ({clientId}) => {
 
 	gameContent.appendChild(playingAgainst);
 	gameContent.appendChild(songPlayingHeader);
+	gameContent.appendChild(guessRightBtn);
 }
 
 const handleGameStart = async (data) => {
 	clearMainContent();
 	handleGameStarting(data);
+	handlePlayNextSong();
 }
 
 const initializeSocketClient = () => {
-	const socket = io.connect('http://localhost:3000', {
+	socket = io.connect('http://localhost:3000', {
 		cors: {
 			origin: '*'
 		}
@@ -55,19 +83,12 @@ const initializeSocketClient = () => {
 	socket.on('game_start', (data) => {
 		console.log('Game started!');
 		handleGameStart(data);
-		const songs = data.songs;
-		const audio = document.getElementById('audioPlayer');
-		audio.src = songs[0];
-		audio.play();
 	})
 
-	console.log(songs);
+	socket.on('correct_guess', () => {
+		handlePlayNextSong();
+	})
 };
-
-const submitSong = (song) => {
-	console.log('Sending song:' + song);
-	socket.emit('guess', song);
-}
 
 const initializeLoader = () => {
 	const loaderContainer = document.getElementById('loaderContainer');
@@ -91,19 +112,6 @@ const initializeMainContent = () => {
 		findOponentButton.style.display = 'none';
 		initializeLoader();
 		initializeSocketClient();
-		const song = songs[0];
-		var queryString = Object.keys(song).map(key => key + '=' + song[key]).join('&');
-
-
-		fetch(`http://localhost:3000/song?${queryString}`)
-			.then(response => response.blob())
-			.then(blob => {
-				const url = URL.createObjectURL(blob);
-				const audio = document.getElementById('audioPlayer');
-				audio.src = url;
-				audio.play();
-			})
-		.catch(error => console.error(error));
 	})
 };
 
